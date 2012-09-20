e1b5125b467329dd1ba6e730bf2c0264310b9a26
Bobik <pushax@ukr.net>
2012-09-21 02:24:22 +0300

    First upload

---
+++ mvn_jsp/src/main/java/com/bobik/mvn_jsp/MyServlet.java
@@ -0,0 +1,36 @@
+package com.bobik.mvn_jsp;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		RequestDispatcher rd = request.getRequestDispatcher("jsp/demo.jsp");
+		request.setAttribute("var", new int[]{1,2,3,4,5,6,777});
+		rd.forward(request, response);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
